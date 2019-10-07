5963d2cccd7695089c431e3269ca8a18834b93fd
jesusbotella <jesus.botella@gmail.com>
2019-10-07 10:31:17 +0200

    Add MVT conversion worker

---
+++ modules/geo-layers/src/mvt-tile-layer/workers/mvt-worker.js
@@ -0,0 +1,55 @@
+import { VectorTile } from '@mapbox/vector-tile';
+import Protobuf from 'pbf';
+
+export class MVTWorker {
+  onMessage(event) {
+    this.processEvent(event).then(message => {
+      postMessage(message);
+    });
+  }
+
+  processEvent(event) {
+    const eventData = event.data;
+
+    return this.getGeoJSONFromTile(eventData)
+        .then(
+          geoJSONData => ({
+            url: eventData.tileURL,
+            features: geoJSONData
+          })
+        );
+  }
+
+  getGeoJSONFromTile({ tileURL, tileProperties }) {
+    return this.requestMVTTile(tileURL)
+               .then(response => this.convertMVTToGeoJSON(response, tileProperties));
+  }
+
+  requestMVTTile(tileURL) {
+    return fetch(tileURL);
+  }
+
+  async convertMVTToGeoJSON(response, tileProperties) {
+    const arrayBuffer = await response.arrayBuffer();
+
+    if (!response || arrayBuffer.byteLength === 0) {
+      return [];
+    }
+
+    const tile = new VectorTile(new Protobuf(arrayBuffer));
+    const features = [];
+
+    for (const layerName in tile.layers) {
+      const vectorTileLayer = tile.layers[layerName];
+
+      for (let i = 0; i < vectorTileLayer.length; i++) {
+        const vectorTileFeature = vectorTileLayer.feature(i);
+        const feature = vectorTileFeature.toGeoJSON(tileProperties.x, tileProperties.y, tileProperties.z);
+
+        features.push(feature);
+      }
+    }
+
+    return features;
+  }
+}
